dde0dc86c8e9241563df0cc01263a494518d66b7
kipling <kip.crossing@gmail.com>
2023-06-13 16:37:41 +1000

    feat: transactionTable

---
+++ src/views/transactionTable.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, type Ref } from "vue";
+import type { TransactionInfo } from "./TransactionView.vue";
+
+
+defineProps<{
+    txList: TransactionInfo[];
+}>();
+
+</script>
+
+<template>
+    <v-table width="100%" height="600px">
+        <thead>
+            <tr>
+                <th class="text-left">Date</th>
+                <th class="text-left">Amount</th>
+                <th class="text-left">Currency</th>
+                <th class="text-left">Direction</th>
+                <th class="text-left">tx Type</th>
+                <th class="text-left">tx Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in txList" :key="index">
+                <td>{{ item.date }}</td>
+                <td>{{ item.amount }}</td>
+                <td>{{ item.currency }}</td>
+                <td>{{ item.direction }}</td>
+                <td>{{ item.tx_type }}</td>
+                <td>{{ item.txAddress }}</td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
