33acb49d9af388a638fbcaed973ec9e0d1cce0be
kipling <kip.crossing@gmail.com>
2023-06-20 22:12:54 +1000

    feat: accountTypes and accType

---
+++ src/models/accountTypes.ts
@@ -0,0 +1,12 @@
+export enum accType {
+  Exchange = "Exchange(AUD)",
+  Client = "Client or Customer",
+  Goods = "Goods or Services",
+  Owned = "Owned Account",
+}
+export const accountTypes: accType[] = [
+  accType.Exchange,
+  accType.Client,
+  accType.Goods,
+  accType.Owned,
+];
