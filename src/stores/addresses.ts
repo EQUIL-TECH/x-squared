ab038624f20a62fb8974277cd690c4503c7157a2
kipling <kip.crossing@gmail.com>
2023-06-25 23:16:19 +1000

    fix: update logic for updating accounts

--- src/stores/addresses.ts
+++ src/stores/addresses.ts
@@ -1,9 +1,10 @@
 import { ref, type Ref } from "vue";
 
 export type AddressData = {
-  address: string | null;
-  name: string | null;
-  accountType: string | null;
+  address: string;
+  name: string;
+  accountType: string;
+  owned: boolean;
 };
 
 export type AddressDataMap = Map<string, AddressData>;
@@ -15,24 +16,30 @@ const ADDRESS_DATA = "addressData";
 export function getAddressData(): AddressDataMap {
   const addMap = localStorage.getItem(ADDRESS_DATA);
   if (addMap) {
-    AddressDataMapRef.value = JSON.parse(addMap);
-    return JSON.parse(addMap);
+    const parsedData: { [key: string]: AddressData } = JSON.parse(addMap);
+    const reconstructedMap: AddressDataMap = new Map(
+      Object.entries(parsedData)
+    );
+    AddressDataMapRef.value = reconstructedMap;
+    return reconstructedMap;
   }
   return new Map<string, AddressData>();
 }
 
 export function saveAddressData(addMap: AddressDataMap) {
+  const obj: { [key: string]: AddressData } = Object.fromEntries(addMap);
   AddressDataMapRef.value = addMap;
-  localStorage.setItem(ADDRESS_DATA, JSON.stringify(addMap));
+  localStorage.setItem(ADDRESS_DATA, JSON.stringify(obj));
 }
 
 export function addAddressData(
   address: string,
   name: string,
-  accountType: string
+  accountType: string,
+  owned: boolean
 ) {
   const addMap = getAddressData();
-  addMap.set(address, { address, name, accountType });
+  addMap.set(address, { address, name, accountType, owned });
   saveAddressData(addMap);
   return addMap;
 }
