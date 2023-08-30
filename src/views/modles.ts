c3e930c8cc7b2b6cdf5e83da193ce80fd19ae4e8
kipling <kip.crossing@gmail.com>
2023-08-30 15:24:20 +1000

    feat: add TransactionInfo type

--- src/views/modles.ts
+++ src/views/modles.ts
@@ -2,3 +2,17 @@ export enum Direction {
   SENT = "sent",
   RECEIVED = "received",
 }
+
+export type TransactionInfo = {
+  tx_type: string;
+  direction: string;
+  amount: number;
+  date: string;
+  currency: string;
+  is_fee: boolean;
+  fee: number;
+  hash: string;
+  txAddress: string;
+  fiatRate?: number | null;
+  fiatAmount?: number | null;
+};
